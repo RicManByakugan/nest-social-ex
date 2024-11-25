e994a8b80b3ebd21645b6f2e5035d82d8516e47c
Ratovonirina <eric.ratovonirina@gmail.com>
2024-11-25 17:50:06 +0330

    main : init strategy file and install dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { MailerModule } from './mailer/mailer.module';
+import { JwtStrategy } from './auth/strategy/strategy.service';
 
 @Module({
   imports: [AuthModule, PrismaModule, MailerModule],
   controllers: [],
-  providers: [],
+  providers: [JwtStrategy],
 })
 export class AppModule {}
---
+++ src/auth/strategy/strategy.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+type Payload = {
+  sub: string;
+  username: string;
+};
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+      ignoreExpiration: false,
+    });
+  }
+
+  async validate(payload: Payload) {
+    
+    return { userId: payload.sub, username: payload.username };
+  }
+}
