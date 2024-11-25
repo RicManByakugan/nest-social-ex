2c1581b40ab8d845882429a1a6762348c7460ec9
Ratovonirina <eric.ratovonirina@gmail.com>
2024-11-25 16:20:14 +0330

    main : generate jwt token for login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,21 @@
-import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { SignUpDto } from './dto/signup.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
 import { MailerService } from 'src/mailer/mailer.service';
 import { SignInDto } from './dto/siginin.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly prismaService: PrismaService,
     private readonly mailerService: MailerService,
+    private readonly jwtService: JwtService,
   ) {}
 
   async signUp(data: SignUpDto) {
@@ -36,13 +42,18 @@ export class AuthService {
     // Vérifier si utilisateur existe
     const user = await this.prismaService.user.findUnique({
       where: { email },
-    })
-    if (user) throw new ConflictException('User not found');
+    });
+    if (!user) throw new ConflictException('User not found');
     // Vérifier si mot de passe correspond
     const match = await bcrypt.compare(password, user.password);
     if (!match) throw new UnauthorizedException('Invalid credentials');
     // Générer token
-    
+    const payload = { sub: user.id, email: user.email };
+    const token = await this.jwtService.sign(payload, {
+      expiresIn: '1d',
+      secret: process.env.JWT_SECRET,
+    });
     // Reponse
+    return { token, user };
   }
 }
