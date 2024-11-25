4b59abcf52a3a7d8767e27a52fbd4e5c8e49fd7d
Ratovonirina <eric.ratovonirina@gmail.com>
2024-11-25 16:00:48 +0330

    main : add nodemailer and service, add signup function service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { MailerModule } from './mailer/mailer.module';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, PrismaModule, MailerModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { SignUpDto } from './dto/signup.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { MailerService } from 'src/mailer/mailer.service';
 
 @Injectable()
 export class AuthService {
-    async signUp(data: SignUpDto){
-        console.log(data);
-        // Vérifier si utilisateur déjà inscrit
-        // Hasher mot de passe
-        // Créer utilisateur
-        // Email
-        // Reponse
-        throw new Error("Not implemented")
-    }
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly mailerService: MailerService,
+  ) {}
+
+  async signUp(data: SignUpDto) {
+    // Vérifier si utilisateur déjà inscrit
+    const { email, password, username } = data;
+    const user = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+    if (user) throw new ConflictException('Email already in use');
+    // Hasher mot de passe
+    const hash = await bcrypt.hash(password, 10);
+    // Créer utilisateur
+    await this.prismaService.user.create({
+      data: { username, email, password: hash },
+    });
+    // Email
+    await this.mailerService.sendSignUpConfirmation(email);
+    // Reponse
+    return { message: 'User created successfully' };
+  }
 }
